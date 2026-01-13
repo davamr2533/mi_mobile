c564a64056208aeab30dab781a2c56c6fa960f5f
davamr2533 <davamr@students.amikom.ac.id>
2026-01-13 17:48:32 +0700

    sepuluh

--- lib/pop_up/login_success.dart
+++ lib/pop_up/login_success.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-//class untuk pop up success
+//class untuk pop up login success
 class LoginSuccess extends StatelessWidget {
   const LoginSuccess({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      backgroundColor: Colors.white, // penting!
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(20)
+        borderRadius: BorderRadius.circular(20),
       ),
       insetPadding: const EdgeInsets.symmetric(horizontal: 100),
-      child: Container(
+      child: Padding(
         padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(40),
-        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -26,23 +23,21 @@ class LoginSuccess extends StatelessWidget {
                 width: 80,
                 height: 80,
                 decoration: BoxDecoration(
-                  color: Color(0xFFECECEC),
-                  borderRadius: BorderRadiusGeometry.circular(100),
+                  color: const Color(0xFFECECEC),
+                  shape: BoxShape.circle,
                 ),
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
-
                     Container(
                       width: 60,
                       height: 60,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadiusGeometry.circular(100),
+                        shape: BoxShape.circle,
                       ),
                     ),
-
-                    Icon(
+                    const Icon(
                       Icons.check_circle_rounded,
                       color: Color(0xFF57D85B),
                       size: 75,
@@ -52,23 +47,19 @@ class LoginSuccess extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 8),
-            Center(
-              child: Text(
-                "Success!",
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
+            Text(
+              "Success!",
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
               ),
             ),
-            Center(
-              child: Text(
-                "Login berhasil!",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  color: Color(0x80000000),
-                ),
+            Text(
+              "Login berhasil!",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                fontSize: 14,
+                color: Color(0x80000000),
               ),
             ),
           ],
@@ -78,23 +69,21 @@ class LoginSuccess extends StatelessWidget {
   }
 }
 
-//class untuk pop up success
+
+//class untuk pop up login success
 class LogOutSuccess extends StatelessWidget {
   const LogOutSuccess({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
+      backgroundColor: Colors.white, // penting!
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(20)
+        borderRadius: BorderRadius.circular(20),
       ),
       insetPadding: const EdgeInsets.symmetric(horizontal: 100),
-      child: Container(
+      child: Padding(
         padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(40),
-        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -103,23 +92,21 @@ class LogOutSuccess extends StatelessWidget {
                 width: 80,
                 height: 80,
                 decoration: BoxDecoration(
-                  color: Color(0xFFECECEC),
-                  borderRadius: BorderRadiusGeometry.circular(100),
+                  color: const Color(0xFFECECEC),
+                  shape: BoxShape.circle,
                 ),
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
-
                     Container(
                       width: 60,
                       height: 60,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadiusGeometry.circular(100),
+                        shape: BoxShape.circle,
                       ),
                     ),
-
-                    Icon(
+                    const Icon(
                       Icons.check_circle_rounded,
                       color: Color(0xFF57D85B),
                       size: 75,
@@ -129,23 +116,19 @@ class LogOutSuccess extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 8),
-            Center(
-              child: Text(
-                "Success!",
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
+            Text(
+              "Success!",
+              style: GoogleFonts.roboto(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
               ),
             ),
-            Center(
-              child: Text(
-                "Anda berhasil LogOut!",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontSize: 14,
-                  color: Color(0x80000000),
-                ),
+            Text(
+              "Anda berhasil Log Out!",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                fontSize: 14,
+                color: Color(0x80000000),
               ),
             ),
           ],
